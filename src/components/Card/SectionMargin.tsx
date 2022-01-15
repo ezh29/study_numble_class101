6e6959851dc71c2f6509d26f3088635a4ba7264a
jihyun <ezh29@naver.com>
2022-01-15 21:23:33 +0900

    Section간 마진 주는 컴포넌트 추가

---
+++ src/components/Card/SectionMargin.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+function SectionMargin() {
+    return (
+        <div css={css`margin:4em 0`}>
+            
+        </div>
+    )
+}
+
+export default SectionMargin
